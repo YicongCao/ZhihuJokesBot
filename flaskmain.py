7292a3d0c570f77a0c927f5d38582cc0cca3c8a4
yicongcao <yicongcao@gmail.com>
2019-03-16 19:20:23 +0800

    web server handle getting joke by keyword

--- flaskmain.py
+++ flaskmain.py
@@ -4,10 +4,12 @@ import pymongo
 import random
 import json
 
+import file2mongo
 import push
 
-from flask import Flask
+from flask import (Flask, request)
 from file2mongo import (client, collection, db)
+from werkzeug.exceptions import abort
 
 
 def create_app(test_config=None):
@@ -16,8 +18,10 @@ def create_app(test_config=None):
 
     @app.route('/joke')
     def get_random_joke():
-        count = collection.count()
-        joke_bson = collection.find()[random.randrange(count)]
+        # count = collection.count()
+        # print('total jokes count: ', count)
+        # joke_bson = collection.find()[random.randrange(count)]
+        joke_bson = file2mongo.random_joke_from_mongo()
         # from bson.json_util import dumps
         # result = dumps(joke_bson)
         # return result
@@ -27,8 +31,35 @@ def create_app(test_config=None):
             "jokes": joke_bson["jokes"],
             "date": joke_bson["date"]
         }
+        print(joke_json)
         # return json.dumps(joke_json, indent=4, ensure_ascii=False)
         joke_md = push.generate_markdown_minimal(joke_json).strip()
+        return joke_md
+
+    @app.route('/jokedict')
+    def get_query_joke():
+        keyword = request.args.get('keyword')
+        if keyword == None or len(keyword) == 0:
+            abort(401)
+        joke_bson = file2mongo.query_joke_from_mongo(keyword)
+        found = True
+        if None == joke_bson:
+            found = False
+            joke_bson = file2mongo.random_joke_from_mongo()
+            # count = collection.count()
+            # print('total jokes count: ', count)
+            # joke_bson = collection.find()[random.randrange(count)]
+        joke_json = {
+            "cover": joke_bson["cover"],
+            "count": joke_bson["count"],
+            "jokes": joke_bson["jokes"],
+            "date": joke_bson["date"]
+        }
+        print(joke_json)
+        if not found:
+            keyword = None
+        joke_md = push.generate_markdown_minimal(
+            joke_json, contains=keyword).strip()
         return joke_md
 
     return app
